a58de584942a0cdd1b46782c7c2122c03f5804f3
grace-jsy <sseo.oee@gmail.com>
2025-08-27 11:49:22 +0900

    add package

---
+++ src/main/java/org/example/schedule3/Schedule3Application.java
@@ -0,0 +1,13 @@
+package org.example.schedule3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Schedule3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Schedule3Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/schedule3/Schedule3ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.schedule3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Schedule3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
